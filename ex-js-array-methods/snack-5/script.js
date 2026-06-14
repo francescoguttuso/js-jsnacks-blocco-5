9c869433052f6c40af73f71446392bd8660a604a
francesco-guttuso <francescoguttuso@gmail.com>
2026-06-14 18:49:47 +0200

    js.-snack-5

--- ex-js-array-methods/snack-5/script.js
+++ ex-js-array-methods/snack-5/script.js
@@ -1,4 +1,8 @@
 const nums = [2, 8, 4, 7, 12, 87];
+const numPari = nums.filter((numero) => {
+  return numero % 2 === 0;
+});
+console.log(numPari);
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
