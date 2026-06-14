91dd0134b2cf8889e2e894407d885aabcf7c4fb9
francesco-guttuso <francescoguttuso@gmail.com>
2026-06-14 18:23:19 +0200

    js-snack-2

--- ex-js-array-methods/snack-2/script.js
+++ ex-js-array-methods/snack-2/script.js
@@ -1,8 +1,12 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
+
+people.forEach((persona) => {
+  console.log(persona.name);
+});
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
